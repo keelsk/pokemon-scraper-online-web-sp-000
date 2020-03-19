4d4bab8c4274e0d953edd4758be1b4d07207addc
Kirsten Keels <keelsk@gmail.com>
2020-03-19 18:43:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,12 +26,12 @@ class Pokemon
     SQL
     
     db.execute(sql, id).map do |row|
-      self.create(row)
+      self.create(row, db)
     end
   end
   
-  def self.create(row)
-    pokemon = self.new(name: row[1], type: row[2])
+  def self.create(row, db)
+    pokemon = self.new(name: row[1], type: row[2], db: db)
     pokemon.id = row[0]
     pokemon
   end
