bb3cd5e9f873560e5797d1216642d5e64ed68dd7
Kirsten Keels <keelsk@gmail.com>
2020-03-19 18:43:17 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -31,7 +31,7 @@ class Pokemon
   end
   
   def self.create(row)
-    pokemon = self.new(name: row[1], type: row[2], db: self.db)
+    pokemon = self.new(name: row[1], type: row[2])
     pokemon.id = row[0]
     pokemon
   end
